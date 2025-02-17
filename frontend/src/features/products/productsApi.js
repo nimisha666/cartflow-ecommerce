299824a8663e8190a4345b99274abcd6a772e05c
nishaD2223 <nimishad2223@gmail.com>
2025-02-17 22:20:32 +0530

    rd

--- frontend/src/features/products/productsApi.js
+++ frontend/src/features/products/productsApi.js
@@ -4,7 +4,7 @@ import { getBaseUrl } from '../../utils/baseURL';
 const productsApi = createApi({
     reducerPath: 'productsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://cartflow-backend-production.up.railway.app/products",
+        baseUrl: "https://cartflow-backend-production.up.railway.app/api/products",
         credentials: 'include'
     }),
     tagTypes: ["Products"],
